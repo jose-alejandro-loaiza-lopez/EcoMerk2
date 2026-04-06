f55c57f3b5121f5cc047a38cfe171bfe2bbb9855
Daniela <daniela.gutierrez01@uceva.edu.co>
2026-04-05 21:12:52 -0500

    feat: desarrollar perfil de usuario con info de cuenta, estadisticas y animacion de entrada (EC-33)

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import '../features/auth/login/login_page.dart';
 import '../features/auth/register/register_page.dart';
 import '../features/home/home_page.dart';
+import '../features/home/chat_page.dart';
+import '../features/home/profile_page.dart';
 import '../features/busqueda/search_page.dart';
 import '../features/favorites/favorites_page.dart';
 
@@ -11,6 +13,8 @@ class AppRoutes {
   static const String home = '/home';
   static const String favorites = '/favorites';
   static const String search = '/search';
+  static const String chat = '/chat';
+  static const String profile = '/profile';
 
   static Map<String, WidgetBuilder> routes = {
     login: (context) => const LoginPage(),
@@ -18,5 +22,7 @@ class AppRoutes {
     home: (context) => const HomePage(),
     favorites: (context) => const FavoritesPage(),
     search: (context) => const SearchPage(),
+    chat: (context) => const ChatPage(),
+    profile: (context) => const ProfilePage(),
   };
 }
